428c9fc06911a7df298bfb031233b1a717014de8
1004yeeun <joycejeoung@gmail.com>
2024-12-18 01:56:57 -0800

    Initial Commit

---
+++ map/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  map
+//
+//  Created by joyce jeoung on 12/18/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
